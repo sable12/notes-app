b7a68140b17dcdce23b7849cfac3ebad9f41897b
mac <mac@macs-MacBook-Pro.local>
2026-09-06 22:23:30 +0100

    another commit

--- backend/src/main/java/com/notesapp/config/CorsConfig.java
+++ backend/src/main/java/com/notesapp/config/CorsConfig.java
@@ -1,5 +1,5 @@
 package com.notesapp.config;
-//testing something
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
